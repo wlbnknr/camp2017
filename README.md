941fb33f774da0d125fdd80fee49656959dc803c
Maks G <Wielebny Knur>
2017-08-15 14:31:26 +0200

    Added login error tooltip and password/login authentication through API

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Chyba lepiej zajmę się kopaniem rowów.
+My Awsum Project
+==================
+
+Awsum project is awsum.
+
+Stuff used
+----------
+
+* Foundation
+* SCSS
+* Git Bash
+* VIM text editor is of best
+* 1337 beginner-level JS h4x0r skillz
--- js/login.js
+++ js/login.js
@@ -2,13 +2,39 @@
 
 	const button = document.getElementsByClassName("submit")[0];
 	const input = document.getElementsByClassName("login")[0];
-
-	//console.log(button);
+	const alert = document.getElementsByClassName("alert")[0];
 
 	button.addEventListener("click", event => {
 		event.preventDefault();
-		input.value == "" ? input.classList.add("error") : location.assign("dashboard.html");
-		console.log(input.className);
+		if (input.value === "") {
+			input.classList.add("error");
+		}
+		else {
+			apirequest(input.value);
+		}
 	});
+
+	function apirequest(password) {
+		$.ajax({
+			// Sends login and password to API
+	 		type: "post",
+			data: {
+	    	login: "efi",
+	    	password: password
+	  		},
+	  
+	  		url: "https://efigence-camp.herokuapp.com/api/login",
+			error: function(response) {
+				const apiResponse = JSON.parse(response.responseText);
+
+				// Changes input & alert styles to display error
+				input.classList.add("error");
+				alert.innerHTML = apiResponse.message;
+				alert.style.display = "block";
+	  		},
+	  		success: function(response) {
+	  		location.assign("dashboard.html");
+	  		}
+	});}
 
 })();
